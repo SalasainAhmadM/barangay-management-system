f4769dbc94345614e6158fa2589da71a81ea11a9
SalasainAhmadM <salasainahmad@gmail.com>
2025-08-30 19:15:13 +0800

    initial commit for folders

---
+++ conn/auth.php
@@ -0,0 +1,16 @@
+<?php
+
+function validateSession($requiredRole = null)
+{
+
+    if (!isset($_SESSION['user_id']) || !isset($_SESSION['user_role'])) {
+        header("Location: ../index.php");
+        exit();
+    }
+
+    if ($requiredRole && $_SESSION['user_role'] !== $requiredRole) {
+        header("Location: ../index.php");
+        exit();
+    }
+}
+?>
